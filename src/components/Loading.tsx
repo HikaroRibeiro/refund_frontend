3108f77509e430ed595474ebfe55fbae2a78dd34
HikaroRibeiro <hhakawama@gmail.com>
2026-02-20 17:35:24 -0300

    ✨ Finish Project
    Finished latest features.

---
+++ src/components/Loading.tsx
@@ -0,0 +1,7 @@
+export function Loading() {
+  return (
+    <div className="w-screen h-screen bg-gray-400 flex justify-center items-center text-gray-100">
+      <h1 className="text-2xl font-bold">Loading...</h1>
+    </div>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,35 @@ import { BrowserRouter } from 'react-router'
 import { EmployeeRoutes } from './EmployeeRoutes'
 import { ManagerRoutes } from './ManagerRoutes'
 import { AuthRoutes } from './AuthRoutes'
+import { Loading } from '../components/Loading'
+
+const loading = false
+
+const session = {
+  user: {
+    role: 'guest',
+  },
+}
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case 'employee':
+        return <EmployeeRoutes />
+      case 'manager':
+        return <ManagerRoutes />
+      default:
+        return <AuthRoutes />
+    }
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+
   return (
     <BrowserRouter>
-      <ManagerRoutes />
+      <Route />
     </BrowserRouter>
   )
 }
